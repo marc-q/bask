931fc2f92c5b151471c9f2b08e0eb192dbe343fd
Marc Volker Dickmann <hq@devdiary.org>
2015-09-12 20:13:23 +0200

    Replaced Dutils

--- lib/dutils.h
+++ lib/dutils.h
@@ -1,1 +1,67 @@
-/home/marcvd/Dokumente/Projekte/DUtils/Trunk/dutils.h
+/*
+ * dutils.h
+ * This file is part of DUtils
+ *
+ * Copyright (C) 2015 - Marc Volker Dickmann
+ *
+ * DUtils is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DUtils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DUtils. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __DUTILS_H__
+#define __DUTILS_H__
+
+/* DUTILS STUFF */
+
+#define DUTILS_VERSION "0.0.1 A"
+
+/* BASH COLORS */
+
+#define BC_TXT_BLACK "\e[0;30m"		/* Black */
+#define BC_TXT_RED "\e[0;31m"		/* Red */
+#define BC_TXT_GREEN "\e[0;32m"		/* Green */
+#define BC_TXT_YELLOW "\e[0;33m"	/* Yellow */
+#define BC_TXT_BLUE "\e[0;34m"		/* Blue */
+#define BC_TXT_PURPLE "\e[0;35m"	/* Purple */
+#define BC_TXT_CYAN "\e[0;36m"		/* Cyan */
+#define BC_TXT_WHITE "\e[0;37m"		/* White */
+
+#define BC_BLD_BLACK "\e[1;30m"		/* Black - Bold */
+#define BC_BLD_RED "\e[1;31m"		/* Red */
+#define BC_BLD_GREEN "\e[1;32m"		/* Green */
+#define BC_BLD_YELLOW "\e[1;33m"	/* Yellow */
+#define BC_BLD_BLUE "\e[1;34m"		/* Blue */
+#define BC_BLD_PURPLE "\e[1;35m"	/* Purple */
+#define BC_BLD_CYAN "\e[1;36m"		/* Cyan */
+#define BC_BLD_WHITE "\e[1;37m"		/* White */
+
+#define BC_UND_BLACK "\e[4;30m"         /* Black - Underline*/
+#define BC_UND_RED "\e[4;31m"           /* Red */
+#define BC_UND_GREEN "\e[4;32m"         /* Green */
+#define BC_UND_YELLOW "\e[4;33m"        /* Yellow */
+#define BC_UND_BLUE "\e[4;34m"          /* Blue */
+#define BC_UND_PURPLE "\e[4;35m"        /* Purple */
+#define BC_UND_CYAN "\e[4;36m"          /* Cyan */
+#define BC_UND_WHITE "\e[4;37m"         /* White */
+
+#define BC_BAK_BLACK "\e[40m"         	/* Black - Background */
+#define BC_BAK_RED "\e[41m"           	/* Red */
+#define BC_BAK_GREEN "\e[42m"         	/* Green */
+#define BC_BAK_YELLOW "\e[43m"        	/* Yellow */
+#define BC_BAK_BLUE "\e[44m"          	/* Blue */
+#define BC_BAK_PURPLE "\e[45m"        	/* Purple */
+#define BC_BAK_CYAN "\e[46m"          	/* Cyan */
+#define BC_BAK_WHITE "\e[47m"         	/* White */
+
+#define BC_TXT_RST "\e[0m"		/* Text Reset */
+
+#endif /* __DUTILS_H__ */
