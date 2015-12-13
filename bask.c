4c465417d1a2391fbc7f684e806b91221e6e964b
Marc Volker Dickmann <hq@devdiary.org>
2015-12-13 20:33:14 +0100

    Fixed that the last config line produced a commented empty line.

--- bask.c
+++ bask.c
@@ -99,7 +99,7 @@ static void bask_init_baskconf (bask_core* tcore)
 		
 		fprintf (baskfile, "# The maximum length of descriptions in characters (0-200; default: 50;)\ntask_description_max=50;\n#\n");
 		fprintf (baskfile, "# The minimum length of the description field in characters (0-200; default: 50;)\ntask_description_min=50;\n#\n");
-		fprintf (baskfile, "# Should longer lines be broken when viewed? (0/1; default: 1)\ntask_description_break=1;\n#\n");
+		fprintf (baskfile, "# Should longer lines be broken when viewed? (0/1; default: 1)\ntask_description_break=1;\n");
 	
 		fclose (baskfile);	
 	}
